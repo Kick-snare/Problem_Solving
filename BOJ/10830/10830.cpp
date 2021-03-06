96a827f19d0322c41f268befa0be5a3ba16dec7c
kick-snare <hyjhyj0901@naver.com>
2021-03-06 17:17:40 +0900

    update

--- BOJ/10830/10830.cpp
+++ BOJ/10830/10830.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+const long long MOD = 1000LL;
+
 int main(){
-    int N, B;
+    int N, B, arr[5][5];
+    long long sum, tmp[5][5], result[5][5];
     cin >> N >> B;
-    for(int N;)
+    for(int i=0;i<N;i++) 
+        for(int j=0;j<N;j++) {
+            cin >> arr[i][j];
+            tmp[i][j] = arr[i][j];
+        }
+
+    for(int i=0;i<B;i++){
+        for(int j=0;j<N;j++){
+            for(int k=0;k<N;k++){
+                sum = 0;
+                for(int l=0;l<N;l++) 
+                    sum += (arr[k][l] * tmp[l][k]) % MOD;
+                result[j][k] = sum % MOD;
+            }
+        }
+        copy(result,result+N*N,tmp);
+    }
+
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++)
+            cout << result[i][j] << ' ';
+        cout << '\n';
+    }
+
     return 0;
 }
---
+++ 조고리즘/Palindrome.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int n;
+
+int is_parlindrome(string str, bool is_rsb){
+    for(int i=0;i<str.length();i++){
+        if()
+    }
+}
+
+int main(){
+    string tmp;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> tmp;
+        int result = is_parlindrome(tmp,true);
+        if(result==1) cout << '1';
+        else if(result==2) cout << '2';
+        else cout << '3'
+    }
+
+
+    return 0;
+}
