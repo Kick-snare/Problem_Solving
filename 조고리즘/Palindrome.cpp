a47e6db6a7f452f377ee8bae9bcc5fdd3acf734a
kick-snare <hyjhyj0901@naver.com>
2021-03-07 01:55:57 +0900

    뭐가문제일까

--- 조고리즘/Palindrome.cpp
+++ 조고리즘/Palindrome.cpp
@@ -1,26 +1,36 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int n;
+// ifstream fin("./sampleData1/2.inp");
+// ofstream fout("./out.txt");
 
-int is_parlindrome(string str, bool is_rsb){
+int is_prdm(string str, bool is_rsb){
     for(int i=0;i<str.length();i++){
-        if()
+        if(str[i]!=str[str.length()-i-1]){
+            if(!is_rsb) return 0;
+            is_rsb = !is_rsb;
+            int len = str.length()-2*i-1;
+            if(is_prdm(str.substr(i,len), is_rsb) || is_prdm(str.substr(i+1,len), is_rsb))
+                return 1;
+        }
     }
+    return 2;
 }
 
 int main(){
-    string tmp;
+    int n;
+    string tmp; 
     cin >> n;
+    cin.ignore();
     for(int i=0;i<n;i++){
-        cin >> tmp;
-        int result = is_parlindrome(tmp,true);
-        if(result==1) cout << '1';
-        else if(result==2) cout << '2';
-        else cout << '3'
+        getline(cin,tmp);
+        int result = is_prdm(tmp,true);
+        if(result==2) cout << '1' << "\n";
+        else if(result==1) cout << '2' << "\n";
+        else cout << '3' << "\n";
     }
-
-
+    // fin.close();
     return 0;
 }
