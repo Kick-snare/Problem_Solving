340df1b328d6d263d49512aa86edb1ef24425a75
kick-snare <hyjhyj0901@naver.com>
2021-03-07 09:43:35 +0900

    palindrome

--- 조고리즘/palin.cpp
+++ 조고리즘/palin.cpp
@@ -3,11 +3,8 @@
 #include <fstream>
 using namespace std;
 
-// ifstream fin("./sampleData1/2.inp");
-// ofstream fout("./out.txt");
-
 int is_prdm(string str, bool is_rsb){
-    for(int i=0;i<str.length();i++){
+    for(int i=0;i<str.length();i++)
         if(str[i]!=str[str.length()-i-1]){
             if(!is_rsb) return 0;
             is_rsb = !is_rsb;
@@ -15,22 +12,21 @@ int is_prdm(string str, bool is_rsb){
             if(is_prdm(str.substr(i,len), is_rsb) || is_prdm(str.substr(i+1,len), is_rsb))
                 return 1;
         }
-    }
     return 2;
 }
 
 int main(){
+    ifstream fin("palin.inp"); ofstream fout("palin.out");
     int n;
     string tmp; 
-    cin >> n;
-    cin.ignore();
+    fin >> n;
     for(int i=0;i<n;i++){
-        getline(cin,tmp);
+        fin >> tmp;
         int result = is_prdm(tmp,true);
-        if(result==2) cout << '1' << "\n";
-        else if(result==1) cout << '2' << "\n";
-        else cout << '3' << "\n";
+        if(result==2) fout << '1' << "\n";
+        else if(result==1) fout << '2' << "\n";
+        else fout << '3' << "\n";
     }
-    // fin.close();
+    fin.close();
     return 0;
 }
